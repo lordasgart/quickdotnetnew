66d3f28875bebeaf74231941a5d9db38693621e2
Markus Malz <markus.malz.ext@siemens.com>
2021-10-05 12:38:34 +0200

    did everything

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+CreateTempDirectory(@"C:\GIT\internal\Scripts\dotnet");
+
+void CreateTempDirectory(string baseDir)
+{
+    var dateTimeFolder = "";
+    var srcDir = Path.Combine(baseDir, dateTimeFolder, "src/ConsoleApp");
+    Directory.CreateDirectory(srcDir);
+}
+
+Process.Start("dotnet", "new console").WaitForExit();
+
+
